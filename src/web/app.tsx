9ffda7991bc3af129ad42dee4ffeb137b2d0585a
Anton Kiselev <anton.kisel@gmail.com>
2018-12-28 01:56:43 +0700

    center & limit width

--- src/web/app.tsx
+++ src/web/app.tsx
@@ -4,7 +4,11 @@ import {Player} from './components/player'
 
 export function App() {
     return (
+        <div className="playerWrapper">
+        <div className="playerWrapperInner">
         <Player/>
+        </div>
+        </div>
     );
 }
 
